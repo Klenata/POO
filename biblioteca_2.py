72476a978455e9dc3dc2858ef07b784c0bf92fef
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-05-08 11:24:53 -0300

    novo exercicio do professor

---
+++ biblioteca_2.py
@@ -0,0 +1,34 @@
+class Banco():
+    def __init__(self, nome, conta):
+        self.nome = nome
+        self.conta = conta
+        self.saldo = 0
+        self.status = False
+
+    def ativarConta(self):
+        if self.status == False:
+            self.status = True
+            print("Conta ativada com sucesso!")
+        else:
+            print("Conta já estava ativada")
+
+    def deposito(self, deposito):
+        if self.status:
+            self.saldo = deposito + self.saldo
+            print(f"Seu saldo atual é de R${self.saldo:.f}")
+        else:
+            print("Sua conta está desativada")
+'''
+    def saque(self):
+
+    def saldo(self):
+        print(self.saldo)
+
+    def desativarConta(self):
+        if self.saldo == 0:
+            self.status = False
+            print("Conta desativada, com sucesso.")
+        else:
+            print("Conta não pode ser desativada")
+
+    def limite(self):'''
---
+++ main_2.py
@@ -0,0 +1,9 @@
+from biblioteca_2 import Banco
+
+usuario1 = Banco("Kleber", "0001")
+usuario2 = Banco("Guilherme", "0002")
+
+usuario1.ativarConta()
+usuario1.deposito(float(input("Valor a depositar: ")))
+usuario1.deposito(float(input("Valor a depositar: ")))
+usuario1.deposito(float(input("Valor a depositar: ")))
