f1aa15ef3cf1f6cb1d69b2c28eb51410dd414bc6
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-05-08 09:42:00 -0300

    fiz o exercicio e consegui fazer a pessoa dormir, falar e comer, e não permitir que um funcione enquanto o outro está funcionando, e mandei ele para tambem...

--- biblioteca.py
+++ biblioteca.py
@@ -8,23 +8,51 @@ class Pessoa():
         self.dormindo = False
 
     def falar(self):
-        if self.dormindo == False and self.comendo == False:
-            self.falando = True
-            print(f"{self.name} começou a falar.")
+        if self.falando == False:
+            if self.dormindo == False and self.comendo == False:
+                self.falando = True
+                print(f"{self.name} começou a falar.")
+            else:
+                print(f"{self.name} não pode falar, está ocupado.")
         else:
-            print("Ele não pode falar, está ocupado.")
+            print(f"{self.name} já está falando")
+
+    def pararFalar(self):
+        if self.falando == True:
+            self.falando = False
+        print(f"{self.name} parou de falar")
 
 
-    def comer(self, comida):
-        if self.dormindo == False and self.falando == False:
-            self.comendo = True
-            print(f"{self.name} começou a comer.")
+    def comer(self):
+        if self.comendo == False:
+            if self.dormindo == False and self.falando == False:
+                self.comendo = True
+                print(f"{self.name} começou a comer.")
+            else:
+                print(f"{self.name} não pode comer, está ocupado.")
         else:
-            print("Ele não pode comer, está ocupado.")
+            print(f"{self.name} já está comendo")
+
+    def pararComer(self):
+        if self.comendo == True:
+            self.comendo = False
+            print(f"{self.name} parou de comer")
+        else:
+            print(f"{self.name} não está comendo")
 
     def dormir(self):
-        if self.comendo == False and self.falando == False:
-            self.dormindo = True
-            print(f"{self.name} começou a dormir.")
+        if self.dormindo == False:
+            if self.comendo == False and self.falando == False:
+                self.dormindo = True
+                print(f"{self.name} começou a dormir.")
+            else:
+                print(f"{self.name} não pode dormir, está ocupado.")
         else:
-            print("Ele não pode dormir, está ocupado.")
+            print(f"{self.name} já está dormindo")
+
+    def acordar(self):
+        if self.dormindo == True:
+            self.dormindo = False
+            print(f"{self.name} acordou")
+        else:
+            
