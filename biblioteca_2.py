0720ba7860306d0d34de32ab81bf928d805c2eac
Kleber Jr. <kleberwagner107@gmail.com>
2025-05-09 10:09:46 -0300

    Finalizei a primeira parte do desafio proposto pelo professor.

--- biblioteca_2.py
+++ biblioteca_2.py
@@ -2,11 +2,11 @@ class Banco():
     def __init__(self, nome, conta):
         self.nome = nome
         self.conta = conta
-        self.saldo = 0
+        self.valor = 0
         self.status = False
 
     def ativarConta(self):
-        if self.status == False:
+        if  not self.status:
             self.status = True
             print("Conta ativada com sucesso!")
         else:
@@ -14,21 +14,32 @@ class Banco():
 
     def deposito(self, deposito):
         if self.status:
-            self.saldo = deposito + self.saldo
-            print(f"Seu saldo atual é de R${self.saldo:.f}")
+            self.valor = deposito + self.valor
+            print(f"Depósito de R${deposito:.2f} Realizado com sucesso.")
         else:
             print("Sua conta está desativada")
 
-    def saque(self):
+    def saque(self, saque):
+        if self.status:
+            if saque > self.valor:
+                print(f"Você só tem R${self.valor} na conta")
+            else:
+                self.valor = self.valor - saque
+        else:
+            print("Sua conta está desativada")
 
     def saldo(self):
-        print(self.saldo)
+        if self.status:
+            saldo = self.valor
+            print(f"R${saldo:.2f}")
+        else:
+            print("Sua conta está desativada")
 
     def desativarConta(self):
-        if self.saldo == 0:
+        if self.valor == 0:
             self.status = False
             print("Conta desativada, com sucesso.")
         else:
             print("Conta não pode ser desativada")
 
-    def limite(self):
+    #def limite(self):
